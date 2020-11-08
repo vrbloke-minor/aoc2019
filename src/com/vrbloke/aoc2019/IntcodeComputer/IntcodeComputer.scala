fed6fc6d3022a45db61c78009e24d65c93248cec
vrbloke <stanislawwis@gmail.com>
2020-11-08 09:16:08 +0100

    readability improved for day 5

--- src/com/vrbloke/aoc2019/IntcodeComputer/IntcodeComputer.scala
+++ src/com/vrbloke/aoc2019/IntcodeComputer/IntcodeComputer.scala
@@ -6,19 +6,11 @@ import scala.math.pow
 class IntcodeComputer(val inputReader: java.util.Scanner) {
   private val memory: Array[Int] = withAnnouncement("Dumping input"){readInput()}
   private var head: Int = 0
-  private val readModes = List(readMode0 _, readMode1 _)
 
   def withAnnouncement[T](msg: String)(x: => T): T = {
     println(msg)
     x
   }
-
-  def read(position: Int): Int = memory(position)
-  def readMode0(arg: Int): Int = read(read(arg))
-  def readMode1(arg: Int): Int = read(arg)
-  def checkMode(argno: Int): Int = (read(head) / pow(10, 1+argno).toInt) % 10
-  def set(position: Int, value: Int): Unit = { memory(position) = value }
-  def dumpMemory(): String = memory.mkString(",")
 
   @scala.annotation.tailrec
   private def readInput(xs: Array[Int] = Array()): Array[Int] = {
@@ -29,32 +21,42 @@ class IntcodeComputer(val inputReader: java.util.Scanner) {
     }
   }
 
+  def dumpMemory(): String = memory.mkString(",")
+  def read(position: Int): Int = memory(position)
+  def set(position: Int, value: Int): Unit = { memory(position) = value }
+  private def readMode0(arg: Int): Int = read(read(arg))
+  private def readMode1(arg: Int): Int = read(arg)
+  private val readModes = List(readMode0 _, readMode1 _)
+  private def checkMode(argno: Int): Int = (read(head) / pow(10, 1+argno).toInt) % 10
+  private def parseParam(argno: Int): Int = readModes(checkMode(argno))(head+argno)
+
   def processOpcode(): Int = {
     val h = head
+    println(s"Processing ${read(h)} head at $h")
     read(h) % 100 match {
       case 99 => return 99
       case 1 =>
-        set(read(h+3), readModes(checkMode(1))(h+1) + readModes(checkMode(2))(h+2))
+        set(read(h+3), parseParam(1) + parseParam(2))
         head += 4
       case 2 =>
-        set(read(h+3), readModes(checkMode(1))(h+1) * readModes(checkMode(2))(h+2))
+        set(read(h+3), parseParam(1) * parseParam(2))
         head += 4
       case 3 =>
         print("Provide input:")
         set(read(h+1), scala.io.StdIn.readInt())
         head += 2
       case 4 =>
-        println(readModes(checkMode(1))(h+1))
+        println(parseParam(1))
         head += 2
       case 5 =>
-        head = if(0 != readModes(checkMode(1))(h+1)) readModes(checkMode(2))(h+2) else head+3
+        head = if(0 != parseParam(1)) parseParam(2) else head+3
       case 6 =>
-        head = if(0 == readModes(checkMode(1))(h+1)) readModes(checkMode(2))(h+2) else head+3
+        head = if(0 == parseParam(1)) parseParam(2) else head+3
       case 7 =>
-        set(read(h+3), if(readModes(checkMode(1))(h+1) < readModes(checkMode(2))(h+2)) 1 else 0)
+        set(read(h+3), if(parseParam(1) < parseParam(2)) 1 else 0)
         head += 4
       case 8 =>
-        set(read(h+3), if(readModes(checkMode(1))(h+1) == readModes(checkMode(2))(h+2)) 1 else 0)
+        set(read(h+3), if(parseParam(1) == parseParam(2)) 1 else 0)
         head += 4
       case _ => println("Incorrect opcode!!")
         return 99
@@ -66,7 +68,6 @@ class IntcodeComputer(val inputReader: java.util.Scanner) {
   private def runCode(): Unit = {
     if(processOpcode() != 99) runCode()
   }
-  def execute(): Unit = runCode()
 
   def solveForNounAndVerb(noun: Int, verb: Int): Int = {
     withAnnouncement("Solving puzzle...") {
@@ -77,7 +78,8 @@ class IntcodeComputer(val inputReader: java.util.Scanner) {
     }
   }
 
-  def findOutput(x: Int): (Int,Int) = {
+  def execute(): Unit = runCode()
+  def findOutputWithNounAndVerb(x: Int): (Int,Int) = {
     for(i <- 0 to 99; j <- 0 to 99) {
       val solver = new IntcodeComputer(new Scanner(memory.mkString(",")).useDelimiter(","))
       if(solver.solveForNounAndVerb(i, j) == x) return (i,j)
--- src/com/vrbloke/aoc2019/day2/main2.scala
+++ src/com/vrbloke/aoc2019/day2/main2.scala
@@ -8,7 +8,7 @@ object main2 extends App {
   val inputReader = new Scanner(new File("input/input.txt"))
   inputReader.useDelimiter(",")
   val mainDelegate = new IntcodeComputer(inputReader)
-  val result = mainDelegate.findOutput(19690720)
+  val result = mainDelegate.findOutputWithNounAndVerb(19690720)
   println(result)
   println(100 * result._1 + result._2)
 }
